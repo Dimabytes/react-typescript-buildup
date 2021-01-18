8d3fd38756972c0b386fd07ca6e4a265e634fe73
Dmitry <41384545+Dimabytes@users.noreply.github.com>
2021-01-18 18:32:58 +0300

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,5 @@ module.exports = {
   printWidth: 100,
   singleQuote: true,
   quoteProps: 'consistent',
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-  bracketSpacing: false,
   useTabs: false,
 };
